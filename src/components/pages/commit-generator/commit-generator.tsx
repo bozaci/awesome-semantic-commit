9adce6e19738535f6057a6a478af8fc1d80c480f
bozaci <yusufonline@yandex.com>
2024-11-29 22:50:26 +0300

    chore(commit-generator): delete google gemini logo in badge

--- src/components/pages/commit-generator/commit-generator.tsx
+++ src/components/pages/commit-generator/commit-generator.tsx
@@ -1,12 +1,9 @@
 import { useTranslations } from 'next-intl';
-import Image from 'next/image';
 
 import Hero from '@/components/ui/hero';
 import Badge from '@/components/ui/badge';
 import CommitGeneratorForm from '@/components/sections/commit-generator-form';
 import CommitGeneratorFAQ from '@/components/sections/commit-generator-faq/commit-generator-faq';
-
-import geminiLogo from '@/assets/images/google-gemini-logo.png';
 
 const CommitGenerator = () => {
   const t = useTranslations('commitGenerator');
@@ -15,12 +12,10 @@ const CommitGenerator = () => {
   return (
     <>
       <Hero>
-        <Badge theme="ghost-gradient-gemini" size="small" isRounded>
-          <>
-            <Image src={geminiLogo} alt="" className="badge__img badge__img--gemini-logo" />
-            &nbsp;
-            {generalT('supportedWithGemini')}
-          </>
+        <Badge theme="ghost-gradient-gemini" size="small" className="d-block" isRounded>
+          {generalT.rich('supportedWithGeminiAndChatGPT', {
+            strong: (chunks: any) => <strong className="text-medium">{chunks}</strong>,
+          })}
         </Badge>
         <Hero.Title>{t('title')}</Hero.Title>
         <Hero.Text>{t('text')}</Hero.Text>
