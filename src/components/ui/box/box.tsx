857c7851ab1ea5a94b7f1dd6718dca2d98b238d2
bozaci <yusufonline@yandex.com>
2024-11-29 22:52:16 +0300

    feat(box): add new parameter line list type

--- src/components/ui/box/box.tsx
+++ src/components/ui/box/box.tsx
@@ -9,7 +9,7 @@ const Box: FC<BoxProps> & {
   Title: FC<{ size?: 'default' | 'medium'; className?: string; children: React.ReactNode }>;
   Text: FC<{ className?: string; children: React.ReactNode }>;
   Group: FC<{ className?: string; children: React.ReactNode }>;
-  List: FC<{ className?: string; children: React.ReactNode }>;
+  List: FC<{ type?: 'circle' | 'line'; className?: string; children: React.ReactNode }>;
   ListItem: FC<{ className?: string; children: React.ReactNode }>;
   Icon: FC<{ color: 'black' | 'red' | 'green'; className?: string; children: React.ReactNode }>;
   Line: FC<{ className?: string }>;
@@ -48,8 +48,12 @@ Box.Group = ({ className, children }) => {
 };
 Box.Group.displayName = 'BoxGroup';
 
-Box.List = ({ className, children }) => {
-  return <ul className={cx('box__list', className)}>{children}</ul>;
+Box.List = ({ type = 'circle', className, children }) => {
+  return (
+    <ul className={cx('box__list', { [`box__list--type-${type}`]: type }, className)}>
+      {children}
+    </ul>
+  );
 };
 Box.List.displayName = 'BoxList';
 
