8d4f174d1af09b9ce7d2e0a41f0cdc8f306e6bd0
bozaci <yusufonline@yandex.com>
2024-12-08 00:12:09 +0300

    feat(not-found-page): add meta data

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,22 @@
+import { Metadata } from 'next';
+import Config from '@/config.json';
+
 import NotFound from '@/components/pages/not-found';
+
+export const metadata: Metadata = {
+  title: Config.meta.notFound.title,
+  description: Config.meta.notFound.description,
+  openGraph: {
+    title: Config.meta.notFound.title,
+    description: Config.meta.notFound.description,
+    images: [],
+  },
+  twitter: {
+    title: Config.meta.notFound.title,
+    description: Config.meta.notFound.description,
+    images: [''],
+  },
+};
 
 const NotFoundPage = () => {
   return <NotFound />;
