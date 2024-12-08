70413698826fde28256012859c4e30a46a6b97a8
bozaci <yusufonline@yandex.com>
2024-12-08 23:47:13 +0300

    feat(link): add new variant, size, className props

--- src/components/ui/link/link.tsx
+++ src/components/ui/link/link.tsx
@@ -1,13 +1,32 @@
 import { FC } from 'react';
 import { LinkProps } from './link.type';
-import Link from 'next/link';
 import { ArrowUpRight } from '@phosphor-icons/react/dist/ssr';
+import Link from 'next/link';
+import cx from 'classnames';
 
 import './link.scss';
 
-const LinkComp: FC<LinkProps> = ({ href, isExternalLink, children }) => {
+const LinkComp: FC<LinkProps> = ({
+  href,
+  variant = 'primary-bottom-line',
+  size = 'default',
+  isExternalLink,
+  className,
+  children,
+}) => {
   return (
-    <Link href={href} target={isExternalLink ? '_blank' : ''} className="link">
+    <Link
+      href={href}
+      target={isExternalLink ? '_blank' : ''}
+      className={cx(
+        'link',
+        {
+          [`link--variant-${variant}`]: variant,
+          [`link--size-${size}`]: size,
+        },
+        className,
+      )}
+    >
       {children}
       {isExternalLink && (
         <span className="link__icon">
--- src/components/ui/link/link.type.ts
+++ src/components/ui/link/link.type.ts
@@ -2,6 +2,9 @@ import React from 'react';
 
 export interface LinkProps {
   href: string;
+  variant?: 'primary-bottom-line' | 'gray';
+  size?: 'default' | 'small' | 'medium';
   isExternalLink?: boolean;
+  className?: string;
   children: React.ReactNode;
 }
