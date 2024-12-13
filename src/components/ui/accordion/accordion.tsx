bc436ee52711694b8ec8be51933c4209caf72e02
bozaci <yusufonline@yandex.com>
2024-12-14 02:39:28 +0300

    feat(accordion): add aria-hidden for accessibility

--- src/components/ui/accordion/accordion.tsx
+++ src/components/ui/accordion/accordion.tsx
@@ -1,4 +1,4 @@
-import { useRef, FC } from 'react';
+import { useState, useRef, FC } from 'react';
 import { AccordionProps } from './accordion.type';
 import { CaretDown } from '@phosphor-icons/react/dist/ssr';
 
@@ -8,20 +8,23 @@ const Accordion: FC<AccordionProps> = ({ title, content, contentPaddingTop }) =>
   const ref = useRef<HTMLDivElement>(null);
   const contentRef = useRef<HTMLDivElement>(null);
   const contentInnerRef = useRef<HTMLDivElement>(null);
+  const [show, setShow] = useState<boolean>(false);
 
   const handleShowContent = () => {
     const innerHeight = contentInnerRef.current?.offsetHeight;
-    const isActive = ref.current?.classList.contains('is-active');
 
     if (!contentRef.current) return console.error(`Accordion: contentRef is not defined.`);
-    if (isActive) {
+    if (show) {
       ref.current?.classList.remove('is-active');
       contentRef.current.style.height = '0px';
+      setShow(false);
+
       return;
     }
 
     ref.current?.classList.add('is-active');
     contentRef.current.style.height = `${innerHeight}px`;
+    setShow(true);
   };
 
   return (
@@ -41,7 +44,7 @@ const Accordion: FC<AccordionProps> = ({ title, content, contentPaddingTop }) =>
         </div>
       </div>
 
-      <div ref={contentRef} className="accordion__content">
+      <div ref={contentRef} className="accordion__content" aria-hidden={!show}>
         <div ref={contentInnerRef} className="accordion__content-inner">
           {content}
         </div>
